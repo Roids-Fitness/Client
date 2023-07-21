56ef3775cab85f1254718b2e64f9bd0f9b28825d
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-07-21 11:53:07 +1000

    feat: created routes using react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>Homepage</h1>}/>
+        <Route path="/class-timetable" element={<h1>Calendar Page</h1>}/>
+        <Route path="/register" element={<h1>Register Page</h1>}/>
+        <Route path="/login" element={<h1>Login Page</h1>}/>
+        <Route path="/class/:id" element={<h1>Class Details Page</h1>}/>
+      </Routes>
     </div>
   );
 }
