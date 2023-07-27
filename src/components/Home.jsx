8c2f64b57b408f4f517332588a4cf891b2de2aa0
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-07-27 22:33:09 +1000

    fix: account for responsive design for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import CarouselContent from './CarouselContent';
 import "./components.css";
 import image1 from "../resources/home-image1.jpeg";
 import image2 from "../resources/home-image2.jpeg";
@@ -9,47 +10,21 @@ function Home() {
   return (
     <Carousel fade>
       <Carousel.Item>
-        <img
-          className="d-block w-100 home-images"
-          src={image1}
-          alt="Rope row"
-        />
+        <img className="d-block w-100 home-images" src={image1} alt="Rope row" />
         <Carousel.Caption>
-          <div className="big-white-text">START YOUR FITNESS JOURNEY AT </div>
-          <div className="big-red-text">ROIDS FITNESS</div>
-          <div>
-            <span className="white-text">Inquire now for a </span>
-            <span className="red-text">30-Day Free </span>
-            <span className="white-text">Membership Trial</span>
-          </div>
+          <CarouselContent />
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100 home-images"
-          src={image2}
-          alt="Treadmill"
-        />
+        <img className="d-block w-100 home-images" src={image2} alt="Weight Bar" />
         <Carousel.Caption>
-          <div className="big-white-text">START YOUR FITNESS JOURNEY AT </div>
-          <div className="big-red-text">ROIDS FITNESS</div>
-          <div>
-            <span className="white-text">Inquire now for a </span>
-            <span className="red-text">30-Day Free </span>
-            <span className="white-text">Membership Trial</span>
-          </div>
+          <CarouselContent />
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100 home-images" src={image3} alt="Yoga" />
         <Carousel.Caption>
-          <div className="big-white-text">START YOUR FITNESS JOURNEY AT </div>
-          <div className="big-red-text">ROIDS FITNESS</div>
-          <div>
-            <span className="white-text">Inquire now for a </span>
-            <span className="red-text">30-Day Free </span>
-            <span className="white-text">Membership Trial</span>
-          </div>
+          <CarouselContent />
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
