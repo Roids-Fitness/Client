76e357d4371bc81ba13da5030fbf8338cd485257
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-08-01 19:50:08 +1000

    feat: functional timetable that navigates to class details page

--- src/components/ClassDetails.jsx
+++ src/components/ClassDetails.jsx
@@ -5,19 +5,52 @@ import image1 from "../resources/class-details-image1.jpeg";
 import { useParams } from "react-router-dom";
 
 function ClassDetails() {
+  
+  const events = [
+    {
+      id: 1,
+      title: "Yoga",
+      description: "Yoga class for beginners",
+      trainer: "John Doe",
+      start: "2023-08-02T12:00:00",
+      end: "2023-08-02T13:00:00",
+    },
+    {
+      id: 2,
+      title: "Pilates",
+      description: "Pilates workout for core strength",
+      trainer: "Jane Smith",
+      start: "2023-08-02T13:00:00",
+      end: "2023-08-02T14:00:00",
+    },
+    {
+      id: 3,
+      title: "Boxing",
+      description: "Boxing training for all levels",
+      trainer: "Mike Johnson",
+      start: "2023-08-05T09:00:00",
+      end: "2023-08-05T10:00:00",
+    },
+    {
+      id: 4,
+      title: "Hit Fit",
+      description: "High-Intensity Training (HIT) session",
+      trainer: "Sarah Williams",
+      start: "2023-08-04T13:00:00",
+      end: "2023-08-04T14:00:00",
+    },
+    {
+      id: 5,
+      title: "Strength Training",
+      description: "Strength training with weights",
+      trainer: "Alex Davis",
+      start: "2023-08-01T13:00:00",
+      end: "2023-08-01T14:00:00",
+    },
+  ];
+  
   const { id } = useParams();
-
-  console.log(id)
-
-  const exampleClass = {
-    id: 1,
-    title: "Pilates",
-    start: new Date(2023, 6, 16, 14, 0),
-    end: new Date(2023, 6, 16, 15, 0),
-    description:
-      "Pilates is a low-impact workout that focuses on improving flexibility, strength, and overall body awareness. The main emphasis in Pilates is on core strength, proper alignment, and controlled movements. It is often done on a mat or with specialized equipment, such as a reformer, which provides resistance to enhance the exercises.",
-    trainer: "Priya Charasu",
-  };
+  const event = events.find((event) => event.id === parseInt(id));
 
   const formatDate = (date) => {
     const options = {
@@ -37,8 +70,8 @@ function ClassDetails() {
     return date.toLocaleTimeString("en-US", options);
   };
 
-  const startTime = new Date(exampleClass.start);
-  const endTime = new Date(exampleClass.end);
+  const startTime = new Date(event.start);
+  const endTime = new Date(event.end);
 
   const formattedDate = formatDate(startTime);
   const formattedTimeRange = `${formatTime(startTime)} - ${formatTime(
@@ -51,13 +84,13 @@ function ClassDetails() {
         <Image src={image1} alt="gym trainer" className="custom-image" />
       </div>
       <div className="word-container">
-        <h1 className="title">{exampleClass.title}</h1>
-        <p>{exampleClass.description}</p>
+        <h1 className="title">{event.title}</h1>
+        <p>{event.description}</p>
         <h1 className="title">Details</h1>
         <p>
           Time: {formattedDate}, {formattedTimeRange}
         </p>
-        <p>Trainer: {exampleClass.trainer}</p>
+        <p>Trainer: {event.trainer}</p>
         <div className="d-flex justify-content-center">
           <Button className="button" type="submit">
             Sign up
--- src/components/ClassTimetable.jsx
+++ src/components/ClassTimetable.jsx
@@ -1,38 +1,87 @@
-import React, {useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./components.css";
-import { DayPilotCalendar } from "@daypilot/daypilot-lite-react";
+import {
+  DayPilotCalendar,
+  DayPilotNavigator,
+} from "@daypilot/daypilot-lite-react";
+import { Link } from "react-router-dom";
 
 function ClassTimetable() {
-
+  const calendarRef = useRef();
   // eslint-disable-next-line
-  const [config, setConfig] = useState({
-    viewType: "Week"
+  const [calendarConfig, setCalendarConfig] = useState({
+    viewType: "Week",
+    durationBarVisible: false,
+    onEventClick: async (args) => {
+      const eventId = args.e.id();
+
+      const url = `/class/${eventId}`;
+
+      window.location.href = url;
+    },
   });
+
+  useEffect(() => {
+    const events = [
+      {
+        id: 1,
+        text: "Yoga",
+        start: "2023-08-02T12:00:00",
+        end: "2023-08-02T13:00:00",
+      },
+      {
+        id: 2,
+        text: "Pilates",
+        start: "2023-08-02T13:00:00",
+        end: "2023-08-02T14:00:00",
+      },
+      {
+        id: 3,
+        text: "Boxing",
+        start: "2023-08-05T09:00:00",
+        end: "2023-08-05T10:00:00",
+      },
+      {
+        id: 4,
+        text: "Hit Fit",
+        start: "2023-08-04T13:00:00",
+        end: "2023-08-04T14:00:00",
+      },
+      {
+        id: 5,
+        text: "Strength Training",
+        start: "2023-08-01T13:00:00",
+        end: "2023-08-01T14:00:00",
+      },
+    ];
+
+    var startDate = new Date(Date.now());
+
+    calendarRef.current.control.update({ startDate, events });
+  }, []);
 
   return (
     <div className="calendar-container">
-      <DayPilotCalendar {...config} />
+      <div>
+        <DayPilotNavigator
+          selectMode={"Week"}
+          showMonths={1}
+          skipMonths={1}
+          startDate={"2023-10-02"}
+          selectionDay={"2023-10-02"}
+          onTimeRangeSelected={(args) => {
+            calendarRef.current.control.update({
+              startDate: args.day,
+            });
+          }}
+        />
+      </div>
+      <div>
+        <Link to="/class/:id">
+        <DayPilotCalendar {...calendarConfig} ref={calendarRef} /></Link>
+      </div>
     </div>
   );
 }
 
 export default ClassTimetable;
-
-  // const classSchedule = [
-  //   {
-  //     id: 1,
-  //     title: 'Pilates',
-  //     start: new Date(2023, 6, 16, 14, 0),
-  //     end: new Date(2023, 6, 16, 15, 0),
-  //     trainer: 'Priya Charasu',
-  //     description: 'Pilates is a low-impact workout that focuses on improving flexibility, strength, and overall body awareness...',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Yoga',
-  //     start: new Date(2023, 6, 17, 10, 30),
-  //     end: new Date(2023, 6, 17, 12, 0),
-  //     trainer: 'Samantha Johnson',
-  //     description: 'Yoga is a practice that combines physical postures, breathing exercises, and meditation...',
-  //   },
-  // ];
