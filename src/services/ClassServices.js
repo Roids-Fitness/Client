a19fbdb875d09b2e522eeac14441c1f792a470ee
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-08-04 11:55:40 +1000

    feat: created class data functions to use in class timetable and class details

--- src/services/ClassServices.js
+++ src/services/ClassServices.js
@@ -1,8 +1,22 @@
+// Fetching class data from the api
+export const fetchClasses = async (apiURL) => {
+  const response = await fetch(`${apiURL}/class`);
+  const data = await response.json();
+  return data;
+};
 
-
-// fetching class data from the api
-export const fetchClasses = async () => { 
-    const response = await fetch(`${API_URL}/class`);
-    const data = await response.json();
-    return data;
-}
+// data conversion model for the class data for the calendar
+export const convertClassData = (data) => {
+    const convertedData = data.map((item) => {
+        const { _id, title, description, startTime, endTime, trainer } = item;
+        return {
+        id: _id,
+        text: title,
+        description,
+        start: startTime,
+        end: endTime,
+        trainer,
+        };
+    });
+    return convertedData;
+};
